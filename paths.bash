fbfdead20f589997d0bf67fed30bd1096eef6bdc
sscott33 <samuel.scott@posteo.net>
2024-04-02 15:28:43 -0500

    sp working w/o completion; need to handle resolution of relative bookmarks

--- paths.bash
+++ paths.bash
@@ -20,7 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-export _PATHS_PATH_DB_FILE="$HOME/.path_db.bash"
+set -x
+export _PATHS_PATH_DB_FILE="/home/esmaosc/lantern_day/2024-04-02/.path_db.bash"
     # stored dictionary is path_db
 export _PATHS_DEFAULT_BM_NAME=_default
 
@@ -94,32 +95,182 @@ _PATHS_SP () {
     . "$_PATHS_PATH_DB_FILE"
     local path
     local bookmark_name
-    case $# in
-        0) # work with pwd
-            path="$(realpath "$(pwd)")"
-            bookmark_name="$_PATHS_DEFAULT_BM_NAME"
-            ;;
-        1) # work with given directory
-            path="$(realpath "$1")"
-            bookmark_name="$_PATHS_DEFAULT_BM_NAME"
-            ;;
-        2) # named given directory
-            path="$(realpath "$1")"
-            bookmark_name="$2"
-            ;;
-        *) # print usage
-            echo "HELP: use this function to save a path; first arg is the path (pwd if not specified); second arg is the reference name"
-            return
-            ;;
-    esac
+    local rel_bookmark_name
+    local func_def
+    local func_name
+    local path_specified=true
 
-    if [[ -d "$path" ]]; then
-        path_db["$bookmark_name"]="$path"
+    declare -a positional_opts
+	while [[ $# -gt 0 && ! "$1" == "--" ]]; do case "$1" in
+		-p|--path)
+            shift
+            path="$1"
+			;;
+        -n|--name)
+            shift
+            bookmark_name="$1"
+            ;;
+        -r|--relative-to)
+            shift
+            rel_bookmark_name="$1"
+            ;;
+        -f|--function)
+            shift
+            func_name="$1"
+            ;;
+        -h|--help)
+            # do help
+            echo "Insert help text here..."
+            return 0
+            ;;
+        [^-]*)
+            positional_opts+=("$1")
+            # is this shift required? or will it break things?
+            # A: it will break things
+            #shift
+            ;;
+	esac; shift; done
+	if [[ "$1" == '--' ]]; then shift; fi
+
+    if [[ ${#positional_opts[@]} -gt 0 ]]; then
+        set -- "${positional_opts[@]}" "$@"
+    fi
+
+    # set the bookmark name if unset
+    if [[ -z "$bookmark_name" ]]; then
+        if [[ -n "$1" ]]; then
+            bookmark_name="$1"
+        else
+            bookmark_name="$_PATHS_DEFAULT_BM_NAME"
+        fi
+    fi
+
+    # set the path if unset
+    if [[ -z "$path" ]]; then
+        if [[ -n "$2" ]]; then
+            path="$2"
+        else
+            path_specified=false
+            path="$(pwd)"
+        fi
+    fi
+
+    # sanity checks; need more of these
+    if [[ -n "$func_name" ]] && $path_specified; then
+        echo "Error: cannot specify both a path and a function" >&2
+        return 1
+    fi
+    if [[ -n "$func_name" && -n "$rel_bookmark_name" ]]; then
+        echo "Error: please define the function-based bookmark first and the bookmark relative to it second; function-based bookmars must be the root of a path" >&2
+        return 1
+    fi
+    #if [[ -n "$func_name" && -z "$bookmark_name" ]]; then
+    #    # refuse default bookmark as a function?
+    #    echo "Error: please specify a bookmark name" >&2
+    #    return 1
+    #fi
+
+    # do we have a func?
+    # do we have a relative?
+    # is it an absolute?
+    if [[ -n "$func_name" ]]; then
+        # get the function definition and make sure it was retrieved correctly
+        func_def="$(declare -pf "$func_name")"
+        if [[ -z "$func_def" || $? -ne 0 ]]; then
+            echo "Error: function does not exist or lacks a definition" >&2
+            return 1
+        fi
+
+        # construct the function bookmark and return early
+        local name_len=${#func_name}
+
+        path_db["$bookmark_name"]="f${name_len}:$func_name$func_def"
+        declare -p path_db > "$_PATHS_PATH_DB_FILE"
+
+        echo "Saved function '$func_name' as '$bookmark_name'"
+        return 0
+
+    elif [[ -n "$rel_bookmark_name" ]]; then
+        # does the relative parent bookmark exist?
+        if [[ -z "${path_db["$rel_bookmark_name"]}" ]]; then
+            echo "Error: the bookmark '$rel_bookmark_name' does not exist" >&2
+            return 1
+        fi
+
+        #local resolved_path="$(realpath --relative-to "$path")"
+        local resolved_path="$(realpath --relative-to "$(_PATHS_PP -r "$bookmark_name")" "$path")"
+        # not yet sure how to check this path until _PATHS_PP resolves it
+        #if [[ ! -e "$resolved_path" ]]; then
+        #    echo "Error: path '$path' ($resolved_path) does not exist" >&2
+        #    return 1
+        #fi
+
+        local name_len=${#bookmark_name}
+        local old_path="$resolved_path"
+        resolved_path="r$name_len:$bookmark_name$resolved_path"
+
+        path_db["$bookmark_name"]="$resolved_path"
+        declare -p path_db > "$_PATHS_PATH_DB_FILE"
+
+        echo "Saved '$path' as '$old_path' relative to '$bookmark_name'"
+        return 0
+
     else
-        echo "Error: '$path' does not exist or is not a directory"
+        local resolved_path="$(realpath "$path")"
+        if [[ ! -e "$resolved_path" ]]; then
+            echo "Error: path '$path' ($resolved_path) does not exist" >&2
+            return 1
+        elif [[ ! -d "$resolved_path" ]]; then
+            echo "Error: path '$path' ($resolved_path) is not a directory" >&2
+            return 1
+        fi
+    fi
+
+    path_db["$bookmark_name"]="$resolved_path"
+    declare -p path_db > "$_PATHS_PATH_DB_FILE"
+
+    if [[ "$bookmark_name" == "$_PATHS_DEFAULT_BM_NAME" ]]; then
+        echo "Saved path '$path' as the default bookmark"
+    else
+        echo "Saved path '$path' as '$bookmark_name'"
+    fi
+
+    return 0
+
+    #####################
+    if [[ -n "$func_def" ]]; then
+        # update this for confirmation text
+        path="$func_name"
+        path_db["$bookmark_name"]="f:$func_name:$func_def"
+        
+    elif [[ -n "$rel_bookmark_name" ]]; then
+        if [[ -z "$path_db["$bookmark_name"]" ]]; then
+            echo "Error: $bookmark_name is not an existing bookmark" >&2
+            return 1
+        fi
+
+
+        if [[ ! -d "$path" ]]; then
+            echo "Error: '$path' does not exist or is not a directory" >&2
+            return 1
+        fi
+
+        path="$(realpath --relative-to "$(_PATHS_PP -r "$bookmark_name")" "$path")"
+
+        path_db["$rel_bookmark_name"]="r:$bookmark_name:$path"
+
+        # remove relevant prefixes
+        path="${path_db["$rel_bookmark_name"]:2}"
+
+    elif [[ -d "$path" ]]; then
+        path_db["$bookmark_name"]="a:$path"
+    else
+        echo "Error: '$path' does not exist or is not a directory" >&2
         return 1
     fi
 
+    # need to handle other cases here
+    bookmark_name="$rel_bookmark_name"
     if [[ "$bookmark_name" != "$_PATHS_DEFAULT_BM_NAME" ]]; then
         echo "saved path '$path' as '$bookmark_name'"
     else
@@ -140,7 +291,12 @@ _PATHS_GP () {
             path="${path_db[$_PATHS_DEFAULT_BM_NAME]}"
             ;;
         1) # work with named directory
-            path="${path_db["$1"]}"
+            #path="${path_db["$1"]}"
+            if ! path="$(_PATHS_PP -r "$1")"; then
+                echo "Error: lookup of '$1' in the bookmark database failed" >&2
+                return 1
+            fi
+            #path="$(realpath -e "$path")"
             ;;
         *) # print usage
             echo "HELP: use this function to go to a named path; will go to the default path if no path is specified; view choices with pp"
@@ -153,10 +309,10 @@ _PATHS_GP () {
             # attempt to cd to the path; cd rarely returns a failure exit code, so print pwd just to be safe
             cd "$path" && echo "working directory is now '$(pwd)'"
         else
-            echo "Error: '$path' does not exist or is not a directory"
+            echo "Error: '$path' does not exist or is not a directory" >&2
         fi
     else
-        echo "Error: '$1' is not a path bookmark"
+        echo "Error: '$1' is not a path bookmark" >&2
         return 1
     fi
 }
@@ -173,7 +329,7 @@ _PATHS_DP () {
             ;;
         *) # delete one or more named paths
             while [[ -n "$1" ]]; do
-                [[ "$1" == "$_PATHS_DEFAULT_BM_NAME" ]] && echo "Error: refusing delete default entry: '$_PATHS_DEFAULT_BM_NAME'" && shift && continue
+                [[ "$1" == "$_PATHS_DEFAULT_BM_NAME" ]] && echo "Error: refusing delete default entry: '$_PATHS_DEFAULT_BM_NAME'" >2 && shift && continue
                 unset path_db["$1"] && echo "deleted reference to '$1'"
                 shift
             done
@@ -190,9 +346,24 @@ _PATHS_PP () {
 
     local key
     local path
+    local value
+    local resolve=false
+    local prefix
+    local parent
+    local func_name
+    local func_def
+
+    # allow user to specify -r or --
+    while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case "$1" in
+        -r)
+            resolve=true
+            ;;
+    esac; shift; done
+    if [[ "$1" == '--' ]]; then shift; fi
+
+
     local starting_argument_count=$#
         # must store this because this variable will decrease with every "shift" call
-
     case $# in
         0) # print all bookmarks
             # print the default bookmark
@@ -204,26 +375,75 @@ _PATHS_PP () {
 
             # print the other bookmarks
             for key in "${!path_db[@]}"; do
-                printf "%s\n" "$key : ${path_db["$key"]}"
+                value="${path_db["$key"]}"
+                value="$(_PATHS_FORMAT_BM "$value")"
+
+                printf "%s\n" "$key : $value"
             done | sort
             ;;
         *) # print the requested bookmark(s); newline separated
+
+            path="${path_db["$1"]}"
 
             while [[ -n "$1" ]]; do
                 path="${path_db["$1"]}"
                 if [[ -z "$path" ]]; then
                     unset path_db[$_PATHS_DEFAULT_BM_NAME]
                     for key in "${!path_db[@]}"; do
-                        [[ "$key" =~ $1 ]] && printf "%s\n" "$key : ${path_db["$key"]}"
+                        value="$(_PATHS_FORMAT_BM "${path_db["$key"]}")"
+                        [[ "$key" =~ $1 ]] && printf "%s\n" "$key : $value"
                     done
                 elif [[ $starting_argument_count -eq 1 ]]; then
                     # if there is only one bookmark and it's exactly named, print path without name -> useful for $(pp <bookmark>)
+                    prefix="${path:0:1}"
+                    if [[ "$prefix" == "f" ]]; then
+                        #path="$(cut -d: -f3 <<<"$path")"
+                        if [[ "$resolve" == "true" ]]; then
+                            #func_name="$(cut -d: -f2 <<<"$path")"
+                            func_name="${path#*:}"
+                            func_def="${func_name#*:}"
+                            func_name="${func_name%:*}"
+                            if ! path="$(eval "$func_def" && "$func_name")"; then
+                                echo Error: failure to execute $func_name, in bookmark $1 >&2
+                                return 1
+                            fi
+                        else
+                            # remove the first two elements of the entry
+                            path="${path#*:}"
+                            path="${path#*:}"
+                        fi
+                    elif [[ "$prefix" == "r" ]]; then
+                        if [[ "$resolve" == "true" ]]; then
+                            parent="$(cut -d: -f2 <<<"$path")"
+                            parent="$(_PATHS_PP -r "$parent")"
+                            path="$(cut -d: -f3 <<<"$path")"
+                            path="$parent/$path"
+                        else
+                            path="${path:2}"
+                        fi
+                    elif [[ "$prefix" == "a" ]]; then
+                        path="${path:2}"
+                    fi
                     printf "%s\n" "$path"
+                    #printf "%s\n" "$(_PATHS_FORMAT_BM "$path")"
                 else
-                    printf "%s\n" "$1 : $path"
+                    printf "%s\n" "$1 : $(_PATHS_FORMAT_BM "$path")"
                 fi
                 shift
-            done | sort
+            done | { [[ -z "$path" ]] && sort -u || cat; }
             ;;
     esac
 }
+
+_PATHS_FORMAT_BM () {
+    local bookmark="$1"
+    if [[ "${bookmark:0:1}" == "f" ]]; then
+        bookmark="$(tr -s [:space:] ' ' <<<"$bookmark" | cut -d: -f3)"
+        bookmark="${bookmark:0:30} ..."
+    else
+        bookmark="${bookmark:2}"
+    fi
+
+    printf "%s" "$bookmark"
+}
+set +x
